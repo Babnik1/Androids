8ca635f7cd2acd13ff6c2518a51c0e549a2f8442
Subject-dot <72896722+Subject-dot@users.noreply.github.com>
2023-11-13 16:29:54 +0300

    Complete first lab

---
+++ app/src/main/java/com/example/lab1/HelloActivity.java
@@ -0,0 +1,48 @@
+package com.example.lab1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class HelloActivity extends Activity {
+
+    int count1, count2;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloact);
+
+        Button button1 = findViewById(R.id.button1);
+        TextView text1 = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.text2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1.setText("НАЖАТО!");
+                count1=count1+1;
+
+                text1.setText("Bottom 1 = " + count1);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setText("НАЖАТО!");
+                count2=count2+1;
+
+                text2.setText("Bottom 2 = " + count2);
+            }
+        });
+
+
+    }
+
+}
